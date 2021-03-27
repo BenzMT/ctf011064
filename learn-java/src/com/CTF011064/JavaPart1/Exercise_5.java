d6e03e76e10d162bffd973f80f4436322404ff0a
Phitsinee <pedassy@hotmail.com>
2021-03-27 12:40:03 +0700

    Completed

--- learn-java/src/com/CTF011064/JavaPart1/Exercise_5.java
+++ learn-java/src/com/CTF011064/JavaPart1/Exercise_5.java
@@ -5,11 +5,11 @@ public class Exercise_5 {
 		System.out.println(sum(10, 20));
 		greeting("Benz");
 	}
-
+	// Return Method
 	public static int sum(int int1, int int2) {
 		return int1 + int2;
 	}
-
+	// Non-return Method
 	public static void greeting(String name) {
 		System.out.println("I'm " + name + ", nice to meet cha! :)");
 	}
